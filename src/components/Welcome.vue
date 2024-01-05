01badccb19bb0ddd3de0ac70fc90d0d5cf167d99
Adam Procter <adamprocter@mac.com>
2024-01-05 17:30:26 +0000

    some edits to base project
    edited readme a little
    add LICENCE file
    removed some additional components and unused code
    Edit HomeView and Welcome Component a little

---
+++ src/components/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>
+    nodenogg.in is free / libre open source software, built in the open, inclusive by design,
+    private by design, humane centred by design, delightful by design, intuitive and decentralised
+    by design.
+  </p>
+  <img
+    src="./images/principles.png"
+    alt="Image of the principles for developing edTech.Namely it should be Free Software, eaxy data export, can be self-run, with data stored locally on devices, modular by design, and founded on data privacy"
+  />
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Welcome from '../components/Welcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Welcome />
   </main>
 </template>
