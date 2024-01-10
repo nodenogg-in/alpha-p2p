b707d619a29672075fd7cc3362ae53c0f01b3636
Adam Procter <adamprocter@mac.com>
2024-01-10 15:14:49 +0000

    added cmd+enter for node creation and can now build

--- src/microcosm/NewNodeCard.vue
+++ src/microcosm/NewNodeCard.vue
@@ -1,49 +1,54 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import MarkdownEditor from './MarkdownEditor.vue';
+import { ref } from 'vue'
+import MarkdownEditor from './MarkdownEditor.vue'
 
 const props = defineProps({
-    onSubmit: {
-        type: Function,
-        required: true
-    }
+  onSubmit: {
+    type: Function,
+    required: true
+  }
 })
 
 const message = ref<string>('')
 
 const handleSubmit = () => {
-    if (message.value) {
-        props.onSubmit(message.value)
-        message.value = ''
-    }
+  if (message.value) {
+    props.onSubmit(message.value)
+    message.value = ''
+  }
 }
 
+document.addEventListener('keydown', (event) => {
+  if (event.metaKey && event.key == 'Enter') {
+    handleSubmit()
+  }
+})
 </script>
 
 <template>
-    <div>
-        <MarkdownEditor :value="message" :onChange="(v: string) => message = v" />
-        <button @click="handleSubmit">Add</button>
-    </div>
+  <div>
+    <MarkdownEditor :value="message" :onChange="(v: string) => (message = v)" />
+    <button @click="handleSubmit">Add</button>
+  </div>
 </template>
 
 <style scoped>
 div {
-    position: relative;
-    display: block;
-    width: 300px;
-    height: 200px;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    border: 1px dashed black;
+  position: relative;
+  display: block;
+  width: 300px;
+  height: 200px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  border: 1px dashed black;
 }
 
 button {
-    position: absolute;
-    bottom: 5px;
-    right: 5px;
-    z-index: 1;
-    cursor: pointer;
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  z-index: 1;
+  cursor: pointer;
 }
 </style>
