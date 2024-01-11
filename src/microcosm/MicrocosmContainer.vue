c3a92baafb4769d60a29bc80212aee5100324717
Adam Procter <adamprocter@mac.com>
2024-01-11 17:07:51 +0000

    adjustments to CSS

--- src/microcosm/MicrocosmContainer.vue
+++ src/microcosm/MicrocosmContainer.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import { watch } from 'vue';
+import { watch } from 'vue'
 import { useAppState } from '../stores/use-app-state'
-import NewNodeCard from './NewNodeCard.vue';
-import NodeCard from './NodeCard.vue';
-import MicrocosmDebug from './MicrocosmDebug.vue';
-import SpatialContainer from '@/components/SpatialContainer.vue';
+import NewNodeCard from './NewNodeCard.vue'
+import NodeCard from './NodeCard.vue'
+import MicrocosmDebug from './MicrocosmDebug.vue'
+import SpatialContainer from '@/components/SpatialContainer.vue'
 
 const props = defineProps({
-    namespace_id: {
-        type: String,
-        required: true
-    },
-    microcosm_id: {
-        type: String,
-        required: true
-    }
+  namespace_id: {
+    type: String,
+    required: true
+  },
+  microcosm_id: {
+    type: String,
+    required: true
+  }
 })
 
 const app = useAppState()
 
 const register = () => {
-    app.registerMicrocosm(props.namespace_id, props.microcosm_id)
+  app.registerMicrocosm(props.namespace_id, props.microcosm_id)
 }
 
 watch(props, register)
@@ -28,64 +28,63 @@ watch(props, register)
 register()
 
 const handleFiles = (files: string[]) => {
-    files.forEach(markdown => {
-        app.createNode({
-            markdown,
-            x: 0,
-            y: 0,
-        })
+  files.forEach((markdown) => {
+    app.createNode({
+      markdown,
+      x: 0,
+      y: 0
     })
+  })
 }
 
 const addRandomNode = (markdown: string) => {
-    app.createNode({
-        markdown,
-        x: 0,
-        y: 0,
-    })
+  app.createNode({
+    markdown,
+    x: 0,
+    y: 0
+  })
 }
-
 </script>
 
 <template>
-    <SpatialContainer @files-dropped="handleFiles" class="microcosm" v-if="!!app.microcosm">
-        <ul>
-            <li>
-                <NewNodeCard :onSubmit="addRandomNode" />
-            </li>
-            <li v-for="[id, node] in app.microcosm?.localNodes" v-bind:key="id">
-                <NodeCard :remote="false" :node="node" />
-            </li>
-        </ul>
-        <ul>
-            <li v-for="[id, node] in app.microcosm?.remoteNodes" v-bind:key="id">
-                <NodeCard :remote="true" :node="node" />
-            </li>
-        </ul>
-    </SpatialContainer>
-    <MicrocosmDebug :microcosm="app.microcosm" />
+  <SpatialContainer @files-dropped="handleFiles" class="microcosm" v-if="!!app.microcosm">
+    <ul>
+      <li>
+        <NewNodeCard :onSubmit="addRandomNode" />
+      </li>
+      <li v-for="[id, node] in app.microcosm?.localNodes" v-bind:key="id">
+        <NodeCard :remote="false" :node="node" />
+      </li>
+    </ul>
+    <ul>
+      <li v-for="[id, node] in app.microcosm?.remoteNodes" v-bind:key="id">
+        <NodeCard :remote="true" :node="node" />
+      </li>
+    </ul>
+  </SpatialContainer>
+  <MicrocosmDebug :microcosm="app.microcosm" />
 </template>
 
 <style scoped>
 div.microcosm {
-    display: flex;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 
 ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
 
 li {
-    width: 300px;
-    height: 200px;
-    padding: 10px;
-    margin: 10px;
-    list-style: none;
+  width: 300px;
+  height: 200px;
+  padding: 10px;
+  margin: 10px;
+  list-style: none;
+  display: inline-flex;
 }
 </style>
 ../stores/app
