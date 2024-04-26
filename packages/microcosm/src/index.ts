15a01911b74870a28e598648e9a26ef392d7bde9
Toby Milner-Gulland <toby@figure.land>
2024-04-26 12:35:18 +0100

    chore: tidied up microcosm exports

--- packages/microcosm/src/index.ts
+++ packages/microcosm/src/index.ts
@@ -1,8 +1,18 @@
-// API
-export { getNodesByType } from './schema/operations/query'
-export { update, type NodeUpdate, type NodeUpdatePayload } from './schema/operations/update'
-export { create, type NodeCreate, type NodeCreatePayload } from './schema/operations/create'
-export { createUpgrade, type NodeUpgrade } from './schema/operations/upgrade'
+// Core Node schema
+export * from './schema/node.types'
+export { isNode, isNodeType, isSpatialNode, isNodeVersion } from './schema/node.guards'
+export const SCHEMA_VERSION = 0
+export { MAX_CHARACTER_COUNT } from './schema/constants'
+
+// Identity schema
+export * from './schema/identity.types'
+export { isIdentity, isIdentityWithStatus } from './schema/identity.guards'
+
+// Microcosm schema
+export * from './schema/microcosm.types'
+export { isSerializedCollection, isSerializedMicrocosm } from './schema/microcosm.guards'
+
+// UUID schema
 export {
   createTimestamp,
   createUuid,
@@ -15,16 +25,14 @@ export {
 } from './schema/uuid.utils'
 export { isValidIdentityID, isValidNodeID, isValidMicrocosmID } from './schema/uuid.guards'
 export type { NodeID, IdentityID, MicrocosmID } from './schema/uuid.types'
+
+// Operations and utilities
+export { getNodesByType } from './schema/operations/query'
+export { update, type NodeUpdate, type NodeUpdatePayload } from './schema/operations/update'
+export { create, type NodeCreate, type NodeCreatePayload } from './schema/operations/create'
+export { createUpgrade, type NodeUpgrade } from './schema/operations/upgrade'
+
+// API types
 export { type MicrocosmAPIConfig, type MicrocosmAPIEvents, MicrocosmAPI } from './MicrocosmAPI'
 export { EditableMicrocosmAPI } from './EditableMicrocosmAPI'
 export { isEditableAPI, type MicrocosmAPIFactory } from './api'
-export * from './schema/node.guards'
-export * from './schema/node.types'
-export * from './schema/identity.types'
-export * from './schema/identity.guards'
-export * from './schema/microcosm.types'
-export * from './schema/microcosm.guards'
-
-export const SCHEMA_VERSION = 0
-
-export { MAX_CHARACTER_COUNT } from './schema/constants'
